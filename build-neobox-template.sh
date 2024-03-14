ff70c1f1877c9fa996ec5e9e2f4df666afcb9170
Ilya Mizus <bigfatbrowncat@gmail.com>
2024-03-14 03:18:20 +0000

    Switched paths to variables

--- build-neobox-template.sh
+++ build-neobox-template.sh
@@ -1,6 +1,9 @@
+echo HOST_DIR=${HOST_DIR}
+echo TARGET_CC=${TARGET_CC}
+
 scons target=template_release \
       neobox_std=c++17 \
-      neobox_pkg_config="/home/user/projects/recalbox/output/host/bin/pkg-config" \
+      neobox_pkg_config="${HOST_DIR}/bin/pkg-config" \
       optimize=speed \
       debug_symbols=no \
       platform=neobox \
@@ -13,10 +16,14 @@ scons target=template_release \
       use_static_cpp=yes \
       arch=arm64 \
       -j5 \
-      CCFLAGS="-I/home/user/projects/recalbox/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include" \
-      CXXFLAGS="-flax-vector-conversions" \
-      CC="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-gcc" \
-      CXX="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-g++" \
-      LD="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-g++" $1
+      CXXFLAGS="${CXXFLAGS} -flax-vector-conversions" \
+      CC="${TARGET_CC}" \
+      CXX="${TARGET_CXX}" \
+      LD="${TARGET_CXX}" $1
 
 #      neobox_custom_renderer=true \
+#      CCFLAGS="-I/home/user/projects/recalbox/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include" \
+#      CXXFLAGS="-flax-vector-conversions" \
+#      CC="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-gcc" \
+#      CXX="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-g++" \
+#      LD="/home/user/projects/recalbox/output/host/bin/aarch64-buildroot-linux-gnu-g++" $1
